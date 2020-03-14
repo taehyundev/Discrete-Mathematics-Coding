08beca0273a97f2742bdf7775a885284adf44a45
taehyundev <50985723+taehyundev@users.noreply.github.com>
2020-03-15 01:05:11 +0900

    BFS(방향성)

---
+++ PrioritySearch/BFS(방향성).cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int vertax, edge;
+int map[50][50];
+bool visit[50];
+
+void BFS(int beginVertax) {
+	queue<int> qStruct;
+	qStruct.push(beginVertax);
+	cout << qStruct.front() << " ";
+	visit[beginVertax] = true;
+	while (!qStruct.empty()) {
+		int pointS = qStruct.front();
+		qStruct.pop();
+		for (int i = 1; i <= vertax; i++) {
+			if (map[pointS][i] == 1 && visit[i] == false) {
+				cout << i << " ";
+				qStruct.push(i);
+				visit[i] = true;
+			}
+		}
+	}
+}
+int main() {
+	cin >> vertax >> edge;
+	// 0 ~ Vertax
+	
+	//방향성 Graph
+	for (int i = 0; i < edge; i++) {
+		int x, y; 
+		cin >> x >> y;
+		map[x][y] = 1;
+	}
+
+	BFS(1);
+}
