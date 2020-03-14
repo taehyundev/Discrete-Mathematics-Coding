1b82d1a2d17f533e96496b9505549f584f8fc70b
taehyundev <50985723+taehyundev@users.noreply.github.com>
2020-03-14 20:36:00 +0900

    DFS(비방향성)

---
+++ PrioritySearch/DFS.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int vertax, edge;
+int map[50][50];
+bool visit[50];
+
+void DFS(int beginVertax) {
+	cout << beginVertax<< " ";
+	visit[beginVertax] = true;
+		for (int i = 1; i <= vertax; i++) {
+			if (map[beginVertax][i] == 1 && visit[i] == false) {
+				visit[i] = true;
+				DFS(i);
+			}
+		}
+	
+}
+int main() {
+	cin >> vertax >> edge;
+	// 0 ~ Vertax
+	
+	//비방향성 Graph
+	for (int i = 0; i < edge; i++) {
+		int x, y;
+		cin >> x >> y;
+		map[x][y] = 1;
+		map[y][x] = 1;
+	}
+
+	DFS(1);
+}
